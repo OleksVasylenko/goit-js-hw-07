6d5b771eaa52be79d01084a54d94d1d0f20e1d40
AVasilenko23 <oleksandrvasylenkojob@gmail.com>
2024-03-24 18:25:15 +0200

    All

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,40 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, "0")}`;
+}
+
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxesContainer = document.getElementById("boxes");
+
+createBtn.addEventListener("click", () => {
+  const amount = document.querySelector("input").valueAsNumber;
+
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+  } else {
+    alert("Please enter a number between 1 and 100.");
+  }
+});
+
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  destroyBoxes();
+
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    boxesContainer.appendChild(box);
+    size += 10;
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
 }
